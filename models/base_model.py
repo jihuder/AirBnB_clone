d83311eed2c066a1df4214fcd767f163b81197b4
angelikrussi <hannamrrussi@gmail.com>
2021-11-04 18:13:02 -0500

    BaseModel update from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -9,11 +9,21 @@ class BaseModel:
     ''' BaseModel:
         We design attributes and methods for other classes
     '''
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         '''Constructor of Base Model'''
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if len (kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key in ["created_at" "updated_at"]:
+                    self.__dict__[key] = datetime.srtptime
+                    (value, '%Y-%m-%dT%H:%M:%S.%f')
+                else:
+                    self.__dict__[key] = value
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         '''Prints the name, class and its dictionary'''
