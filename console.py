414d23ceaf4f3c6c488cb76bfd027c6bad169702
jihuder <julioarenas85@gmail.com>
2021-11-06 18:55:22 -0500

    [Feat ~ Console](Update Console)

--- console.py
+++ console.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/python3
 """Module: console.py"""
 import cmd
@@ -207,33 +206,3 @@ class HBNBCommand(cmd.Cmd):
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
-=======
-#!/usr/bin/python3
-"""Module: console.py"""
-import cmd
-import models
-import re
-from models.base_model import BaseModel
-
-
-class HBNBCommand(cmd.Cmd):
-    """ command line interpreter"""
-
-    prompt = '(hbnb) '
-
-    def do_quit(self, line):
-        """ Quit command to exit the program\n """
-        return True
-
-    def do_EOF(self, line):
-        """ Quit command to exit the program\n """
-        print()
-        return True
-
-    def emptyline(self):
-        """ Handle empty lines"""
-        return
-
-if __name__ == '__main__':
-    HBNBCommand().cmdloop()
->>>>>>> a73fa7862399422281753b24f76ab1c76acce4db
