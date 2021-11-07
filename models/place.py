50ff391003deead419b53fb5523ea71bed94b49b
angelikrussi <hannamrrussi@gmail.com>
2021-11-06 19:31:24 -0500

    More classes

---
+++ models/place.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Module: place.py"""
+from models.base_model import BaseModel
+
+
+class Place(BaseModel):
+    """Place class
+        Public class attributes:
+            city_id: string - empty string: it will be the City.id
+            user_id: string - empty string: it will be the User.id
+            name: string - empty string
+            description: string - empty string
+            number_rooms: integer - 0
+            number_bathrooms: integer - 0
+            max_guest: integer - 0
+            price_by_night: integer - 0
+            latitude: float - 0.0
+            longitude: float - 0.0
+            amenity_ids: list of string - empty list"""
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
+
+    def __init__(self, *args, **kwargs):
+        """Class constructor for Place"""
+        super().__init__(*args, **kwargs)
